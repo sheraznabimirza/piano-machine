f2f590ebe127ef90e6817e9ef90d5a837c2c8df2
sheraznabimirza <sheraznabimirza@gmail.com>
2021-01-18 20:54:42 +0500

    First version of the piano machine.

---
+++ src/SoundButton.js
@@ -0,0 +1,70 @@
+import { useState, useRef } from "react"
+
+const sounds = [
+    {
+        key: 'Q',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    },
+    {
+        key: 'W',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    },
+    {
+        key: 'E',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    },
+    {
+        key: 'A',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    },
+    {
+        key: 'S',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+        key: 'D',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+        key: 'Z',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+        key: 'X',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+        key: 'C',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    },
+];
+
+export function SoundButton() {
+
+    return (
+        <div className="container">
+            <h1>Play a sound</h1>
+            {sounds.map((sound, ind) => (
+            <PianoKey letter={sound.key} key={ind} audio={sound.mp3} />))}
+        </div>
+    );
+}
+
+function PianoKey(props) {
+
+    const audioRef = useRef(null);
+
+    const playSound = () => {
+        audioRef.current.play();
+    }
+
+    return (
+        <div className="drum-pad" onClick={playSound}>
+            {props.letter}
+            <audio ref={audioRef} src={props.audio} id={props.key}></audio>
+        </div>
+        
+    );
+
+}
+
